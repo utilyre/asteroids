26e9841f3b9e0cea0b2368432248c9582f5ab248
Amirabbas Fazelinia <utilyre@gmail.com>
2025-03-01 18:14:33 +0200

    feat(player): implement movement

--- player.py
+++ player.py
@@ -18,8 +18,17 @@ class Player(CircleShape):
         if keys[pygame.K_d]:
             self.rotate(dt)
 
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += PLAYER_SPEED * dt * forward
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
