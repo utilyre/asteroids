362f034e67bb0f7b761ce6f7b1f9a3e10462e49c
Amirabbas Fazelinia <utilyre@gmail.com>
2025-03-06 10:02:36 +0200

    make a Game object to contain everything

--- game/main.py
+++ game/main.py
@@ -1,3 +1,4 @@
+import logging
 import pygame
 import sys
 from asteroid import Asteroid
@@ -9,67 +10,91 @@ import socket
 from network import send_message
 
 def main():
-    print("Creating a connection to the server")
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        print("Establishing the connection")
-        sock.connect(("localhost", 3000))
-        print("Connected to remote server")
+    logging.basicConfig(level=logging.DEBUG)
 
-        print("Initializing pygame")
-        success, failure = pygame.init()
-        print(f"Successfully started {success} modules")
-        if failure > 0:
-            print(f"Failed to start {failure} modules")
+    logging.info("initializing pygame")
+    pygame_num_success, pygame_num_failure = pygame.init()
+    logging.info("initialized pygame", extra={"num_success": pygame_num_success, "num_failure": pygame_num_failure})
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    logging.info("set up display", extra={"width": SCREEN_WIDTH, "height": SCREEN_HEIGHT})
 
-        print("Setting up display")
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        print(f"Screen width: {SCREEN_WIDTH}")
-        print(f"Screen height: {SCREEN_HEIGHT}")
+    # all actions must be in player, right?
+    # oh shit, asteroid field also has actions
+    # but no, the server should spawn the asteroids
 
-        updatable = pygame.sprite.Group()
-        drawable = pygame.sprite.Group()
-        asteroids = pygame.sprite.Group()
-        shots = pygame.sprite.Group()
-        Player.containers = (updatable, drawable) # TODO: How tf does this work?
-        Shot.containers = (updatable, drawable, shots)
-        Asteroid.containers = (updatable, drawable, asteroids)
-        AsteroidField.containers = (updatable)
+    with Game(screen) as g:
+        g.start()
 
+SERVER_HOST = "localhost"
+SERVER_PORT = 3000
+
+class Game:
+    # connect to server
+    # contain all state
+    # init, start, exit
+
+    def __init__(self, screen):
+        logging.info("initializing game object")
+
+        self.screen = screen
+
+        self.clock = pygame.time.Clock()
+        self.dt = 0
+
+        self.updatable = pygame.sprite.Group()
+        self.drawable = pygame.sprite.Group()
+        self.asteroids = pygame.sprite.Group()
+        self.shots = pygame.sprite.Group()
+        Player.containers = (self.updatable, self.drawable) # TODO: How tf does this work?
+        Shot.containers = (self.updatable, self.drawable, self.shots)
+        Asteroid.containers = (self.updatable, self.drawable, self.asteroids)
+        AsteroidField.containers = (self.updatable)
+
+        self.sock = None
+
+        logging.info("game object initialized")
+
+    def __enter__(self):
+        logging.info("connecting to server via tcp")
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((SERVER_HOST, SERVER_PORT))
+        logging.info("successfully connected to server")
+        return self
+
+    def __exit__(self, exec_type, exec_value, traceback):
+        logging.info("closing tcp connection to the server")
+        self.sock.close()
+
+    def start(self):
         player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-        asteroid_field = AsteroidField(sock)
+        asteroid_field = AsteroidField(self.sock)
+        logging.info("initial game entities spawned")
 
-        # all actions must be in player, right?
-        # oh shit, asteroid field also has actions
-        # but no, the server should spawn the asteroids
-
-        clock = pygame.time.Clock()
-        dt = 0
-
-        print("Starting Asteroids!")
+        logging.info("starting game loop")
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
 
-            updatable.update(dt)
+            self.updatable.update(self.dt)
 
-            for asteroid in asteroids:
+            for asteroid in self.asteroids:
                 if player.collides_with(asteroid):
-                    print("Game Over!")
+                    logging.info("player died, game is over")
                     sys.exit()
 
-            for shot in shots:
-                for asteroid in asteroids:
+            for shot in self.shots:
+                for asteroid in self.asteroids:
                     if shot.collides_with(asteroid):
                         shot.kill()
                         asteroid.split()
 
-            screen.fill("black")
-            for sprite in drawable:
-                sprite.draw(screen)
+            self.screen.fill("black")
+            for sprite in self.drawable:
+                sprite.draw(self.screen)
             pygame.display.flip()
 
-            dt = clock.tick(60) / 1000
+            self.dt = self.clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
